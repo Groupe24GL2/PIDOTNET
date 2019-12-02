558ba0669392628bcb3dc17a016efb06024ad112
NadaAc <nada.achour0641@gmail.com>
2019-12-02 11:04:50 +0100

    add training model

---
+++ PIDOTNET.WEB/Models/Training.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PIDOTNET.WEB.Models
+{
+    public class Training
+    {
+        public int id { get; set; }
+        public string category { get; set; }
+        public string description { get; set; }
+        public DateTime? endDate { get; set; }
+        public string location { get; set; }
+        public string name { get; set; }
+        public int nbr_participants { get; set; }
+        public DateTime? startDate { get; set; }
+    }
+}
