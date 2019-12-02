f432970ed4f688b8dc59a9f7273d6a71b893fef0
yassinetaktak <yassinetaktak@users.noreply.github.com>
2019-12-02 02:26:49 +0100

    test branch taktak

---
+++ PIDOTNET.DATA/DataModel/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIDOTNET.DATA.DataModel
+{
+    class Class1
+    {
+        ///////fgdjhf
+        //rghhftfyhgjffdfdf
+    }
+}
