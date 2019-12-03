3116d446d2a34d31993a315d3a9f0334b4fd13eb
HoussemBouzid <houssem.bouzid@esprit.tn>
2019-12-03 18:06:30 +0100

    affichage mission

--- PIDOTNET.WEB/Controllers/MissionController.cs
+++ PIDOTNET.WEB/Controllers/MissionController.cs
@@ -1,7 +1,7 @@
-﻿using System;
+﻿using PIDOTNET.DATA.DataModel;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Net.Http;
 using System.Web.Mvc;
 
 namespace PIDOTNET.WEB.Controllers
@@ -11,7 +11,30 @@ namespace PIDOTNET.WEB.Controllers
         // GET: Mission
         public ActionResult Index()
         {
+            HttpClient Client = new HttpClient();
+            Client.BaseAddress = new Uri("http://localhost:9080/PIDEV4GL2-web/");
+            Client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = Client.GetAsync("api/all").Result;
+            if (response.IsSuccessStatusCode)
+            {  
+
+                
+                ViewBag.result = response.Content.ReadAsAsync<IEnumerable<mission>>().Result;
+            }
+
+            else
+            {
+                ViewBag.result = "error";
+            }
+
             return View();
         }
+
+
+
+
+
+
+
     }
 }
